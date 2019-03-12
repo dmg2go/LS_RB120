8ddfbbfbb281c3d852727b8227015ea33acca735
dmg2go <dmg2go@gmail.com>
2019-03-11 22:01:30 -0400

    update: partial implementation of computer player logic to pick competitive moves. Most code in select_opponent_play method of Player class.

--- RB120_lsn5_ticTacToe.rb
+++ RB120_lsn5_ticTacToe.rb
@@ -8,7 +8,7 @@
 require 'pry'
 
 class Game
-  GAME_PROMPTS = {new_game: "Want to a_play Tic Tac Toe?", new_a_play: "Select a cell to mark ... (by number)"}
+  GAME_PROMPTS = {new_game: "Want to a_play Tic Tac Toe?", new_move: "Select a cell to mark ... (by number)"}
   VALID_USER_RESPONSES = [:Yes, :No, :Y, :N, :y, :n]
   PLAYER_TYPES = [:user, :opponent]
 
@@ -16,8 +16,8 @@ class Game
 
   def initialize
     @board = Board.new
-    @user = Player.new(:user, 'U', "active")
-    @opponent = Player.new(:opponent, 'C', "active")
+    @user = Player.new(:user, 'U')
+    @opponent = Player.new(:opponent, 'C')
     self.play_new_game?
   end
 
@@ -47,12 +47,12 @@ class Game
   def run_game
     @game_state = "active"
     current_player = @user
-    restore_board
+    reset_board
 
     # main game play loop
     while @game_state == 'active' do
       if current_player == @user
-        user.move(@board, GAME_PROMPTS[:new_a_play])
+        user.move(@board, GAME_PROMPTS[:new_move])
       elsif current_player == @opponent
         opponent.move(@board, '')
       end
@@ -74,7 +74,7 @@ class Game
     current_player == @user ? @opponent : @user
   end
 
-  def restore_board
+  def reset_board
     @board.restore_board 
     show_board
   end
@@ -101,23 +101,20 @@ class Game
 end
 
 class Player
-  # unused 3/8/19 VALID_PLAYER_STATUS = ["winner", "loser", "tied"]
-  attr_reader :a_play, :type, :marker # , :status
+  attr_reader :a_play, :type, :marker
 
-  def initialize(type, marker, status)
+  def initialize(type, marker)
     @type = type
     @marker = marker
-    # @status = status  # unused
   end
 
   def move(game_board, move_prompt)
-    #this_play = nil
     if self.type == :user
       this_play = select_user_play(game_board, move_prompt)
     elsif self.type == :opponent
-      this_play = select_opponent_play(game_board)
+      this_play = select_opponent_play(game_board, 'U')
     end
-    game_board.mark_square_at(this_play, self)
+    game_board.mark_square_at(this_play, self.marker)
 
   end
 
@@ -129,7 +126,7 @@ class Player
       @a_play = gets.chomp!
 
       if valid_play?(@a_play, game_board.available_squares)
-        consume_play(@a_play, game_board.available_squares) # returns deleted item @a_play
+        consume_play(@a_play, game_board.available_squares)
       else
         puts "play is invalid: choose a valid play!"
         @a_play = nil
@@ -138,19 +135,63 @@ class Player
     @a_play
   end
 
-  def select_opponent_play(game_board)
+  def select_opponent_play(game_board, rival_mark)
     @a_play = nil
     until @a_play do
-      @a_play = game_board.available_squares.sample
-      # binding.pry
+      game_board.winable_sets.each do |ws|
+        my_mark_count = 0
+        rival_mark_count = 0
+        #binding.pry
+        ws.each do |square_id|
+          if rival_mark == game_board.get_square_at(square_id)
+            binding.pry
+            rival_mark_count += 1
+          end
+          if self.marker == game_board.get_square_at(square_id)
+            binding.pry
+            my_mark_count += 1
+          end
+          # choose third unused square as play, to block or win
+          binding.pry if rival_mark_count == 2
+          if (rival_mark_count == 2 || my_mark_count == 2) && game_board.available_squares.include?(square_id)
+              #binding.pry
+              @a_play = square_id
+          end
+          binding.pry if rival_mark_count == 1
+          if rival_mark_count == 1 && game_board.available_squares.include?('5') && ws.include?(5)
+            @a_play = '5'
+            #binding.pry
+          end
+          binding.pry if rival_mark_count == 1
+          if rival_mark_count == 1 && @a_play == nil && game_board.available_squares.include?(square_id)
+            #binding.pry
+            @a_play = square_id
+          end
+          binding.pry if my_mark_count == 1
+          if my_mark_count == 1 && @a_play == nil && game_board.available_squares.include?(square_id)
+            binding.pry
+            @a_play = square_id
+          end
+          #binding.pry
+        end # end inner each square_id do
+        binding.pry
+      end # end outer each winable_set do
+      binding.pry
+ 
+      if @a_play == nil
+        @a_play = game_board.available_squares.sample
+      end
+
+      binding.pry
       if valid_play?(@a_play, game_board.available_squares)
         puts "Computer selects #{@a_play} ..."
         consume_play(@a_play, game_board.available_squares)
       else
         @a_play = nil
       end
+      binding.pry
     end
-    # binding.pry
+    binding.pry
     @a_play
   end
 
@@ -164,13 +205,12 @@ class Player
 end
 
 class Board
-  attr_reader :available_squares
+  attr_reader :available_squares, :winable_sets, :squares
 
   def initialize
-    @available_squares = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     @squares = {}
-    (1..9).each {|key| @squares[key] = Square.new(key.to_s)}
-    @win_sets = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]]
+    @winable_sets = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]]
+    restore_board
   end
 
   def show
@@ -198,19 +238,19 @@ class Board
     @squares[key]
   end
 
-  def mark_square_at(key, player)
-    @squares[key.to_i] = player.marker
+  def mark_square_at(key, marker)
+    @squares[key.to_i] = marker
     p @squares[key.to_i]
     # binding.pry
   end
 
   def judge_score
-    @win_sets.each do |winning_array|
+    @winable_sets.each do |winning_array|
       if @squares[winning_array[0]] == @squares[winning_array[1]] && @squares[winning_array[1]] == @squares[winning_array[2]]
         return @squares[winning_array[0]]
       end
       # all winning sets contain marks of both players
-      if @win_sets.all?{|w_a| w_a.any?("U") && w_a.any?("C")} 
+      if @winable_sets.all?{|w_a| w_a.any?("U") && w_a.any?("C")} 
         binding.pry
         return "T"
       end
@@ -228,6 +268,8 @@ class Square
   def to_s
     @marker
   end
+
+
 end
 
 g = Game.new
